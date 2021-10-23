be9d1fd5c19e118c2da81b4a684c8caba9fe58ee
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-10-23 23:43:02 +0530

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,7 +2,7 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 Hᴇʟʟᴏ, \n\nAᴍ **{BOT_NAME}** \nI Cᴀɴ Sᴛʀᴇᴀᴍ Lɪᴠᴇs, Rᴀᴅɪᴏs, YᴏᴜTᴜʙᴇ Vɪᴅᴇᴏs & Tᴇʟᴇɢʀᴀᴍ Aᴜᴅɪᴏ / Vɪᴅᴇᴏ Fɪʟᴇs Oɴ Vᴏɪᴄᴇ Cʜᴀᴛ Oғ Tᴇʟᴇɢʀᴀᴍ Gʀᴏᴜᴘs. Lᴇᴛ's Eɴɪᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs 😉! \n\n**Mᴀᴅᴇ Wɪᴛʜ ❤️ Bʏ [🎥𝚂𝚃𝙾𝚁𝚈 𝚃𝙸𝙼𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 #𝙲𝙷𝙰𝚃𝙸𝙽𝙶💬](https://telegram.me/storytym)!** 👑"
+START_TEXT = f"👋🏻 Hᴇʟʟᴏ, \n\nAᴍ **{BOT_NAME}** \nI Cᴀɴ Sᴛʀᴇᴀᴍ Lɪᴠᴇs, Rᴀᴅɪᴏs, YᴏᴜTᴜʙᴇ Vɪᴅᴇᴏs & Tᴇʟᴇɢʀᴀᴍ Aᴜᴅɪᴏ/Vɪᴅᴇᴏ Fɪʟᴇs Oɴ Vᴏɪᴄᴇ Cʜᴀᴛ Oғ Tᴇʟᴇɢʀᴀᴍ Gʀᴏᴜᴘs. Lᴇᴛ's Eɴɪᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs 😉! \n\n**Mᴀᴅᴇ Wɪᴛʜ ❤️ Bʏ [🎥𝚂𝚃𝙾𝚁𝚈 𝚃𝙸𝙼𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 #𝙲𝙷𝙰𝚃𝙸𝙽𝙶💬](https://telegram.me/storytym)!** 👑"
 HELP_TEXT = f"""
 🛠-- **Sᴇᴛᴛɪɴɢ Uᴘ Bᴏᴛ**:--
 
@@ -17,4 +17,6 @@ HELP_TEXT = f"""
 \u2022 /endstream - Eɴᴅ Sᴛʀᴇᴀᴍ & Lᴇғᴛ VC
 \u2022 /restart -  Rᴇsᴛᴀʀᴛ Bᴏᴛ (Sᴜᴅᴏ Oɴʟʏ)
 """
-ABOUT_TEXT = f"💡-- **DARK LORD**:-- \n\n❍ ᴄʀᴇᴀᴛᴏʀ : [ᴅᴀʀᴋ ʟᴏʀᴅ](t.me/VAMPIRE_KING_NO_1)"
+ABOUT_TEXT = f"""
+💡-- **DARK LORD**:-- \n\n❍ ᴄʀᴇᴀᴛᴏʀ : [ᴅᴀʀᴋ ʟᴏʀᴅ](t.me/VAMPIRE_KING_NO_1)
+\u2022 **BTS ARMYS 🄵🄰🄽🄲🄻🅄🄱**:-- \n\n❍ ɢʀᴏᴜᴘ : [BTS ARMYS 🄵🄰🄽🄲🄻🅄🄱](https://t.me/BTS_GROUP_AMRY)"
